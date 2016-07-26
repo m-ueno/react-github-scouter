5e35610a1e083e8ac8c5bf1b87dcaeedc758d8ee
m-ueno <m-ueno@github.com>
2016-07-26 22:27:36 +0900

    add Scouter

---
+++ src/Scouter.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class ScouterCard extends React.Component {
+  render() {
+    console.log(this.props);
+    const { name, atk, int, agi } = this.props.data;
+    return (<div>
+      <p>NAME: {name}</p>
+      <p>ATTACK:{atk}, INTELLIGENCE:{int} AGILITY:{agi}</p>
+    </div>);
+  }
+}
+ScouterCard.propTypes = { data: React.PropTypes.object };
+
+export default class Scouter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scouterData: [{}],
+      error: false,
+    };
+  }
+  componentDidMount() {
+    const uri = '/scouterData.json';
+    fetch(uri)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ scouterData: json });
+      })
+      .catch(err => {
+        console.log('error', err);
+        this.setState({ error: err });
+      })
+      ;
+  }
+  render() {
+    const cards = this.state.scouterData.map(e => <ScouterCard data={e} key={e.name} />);
+    return (<div>
+      {cards}
+      {
+        this.state.error
+        ? <p>this.state.error</p>
+        : null
+      }
+    </div>);
+  }
+}
